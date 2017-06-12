4caad090489b94e4c94eeede0ac7c72e75208ec9
Pawel Skaruz <paw3lx@gmail.com>
2017-06-13 00:31:34 +0200

    added commetns to IStegoAlgorithm methods

--- src/StegoCoreLib/Algorithms/IStegoAlgorithm.cs
+++ src/StegoCoreLib/Algorithms/IStegoAlgorithm.cs
@@ -19,9 +19,34 @@ namespace StegoCore.Algorithms
     {
         protected int SecretDataLength = 32;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stegoImage"></param>
+        /// <returns></returns>
         public abstract byte[] Decode(Image stegoImage);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="baseImage"></param>
+        /// <param name="secret"></param>
+        /// <returns></returns>
         public abstract Image Embed(Image baseImage, SecretData secret);
+
+        /// <summary>
+        /// Checks if embed is possible
+        /// </summary>
+        /// <param name="image">Image</param>
+        /// <param name="secretLength">length of the secret to embed</param>
+        /// <returns>possibility of embeding</returns>
         public abstract bool EmbedPossible(Image image, int secretLength);
+
+        /// <summary>
+        /// Reads emended secret length in image
+        /// </summary>
+        /// <param name="stegoImage">Image with emended message</param>
+        /// <returns>secret length</returns>
         public abstract int ReadSecretLength(Image stegoImage);
     }
 }
